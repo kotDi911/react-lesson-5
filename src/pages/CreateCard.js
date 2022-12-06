dc0f74091392adcc34cd7cbca2e93f833fc5a186
= <=>
2022-12-06 14:41:47 +0200

    v3

--- src/pages/CreateCard.js
+++ src/pages/CreateCard.js
@@ -14,7 +14,7 @@ class CreateCard extends Component {
             user: "",
             card: "mastercard",
             stats: [],
-            date: new Date().toLocaleDateString("en-US"),
+            date: new Date().toLocaleDateString("en-US", { year: 'numeric', month: '2-digit', day: '2-digit' }),
             onSubmit: this.onSubmit.bind(this),
             onChange: this.changeValue.bind(this),
             error: true,
